552a3ff416f9aca7981d415dac20e842204c9e49
Evan Boehs <evan@boehs.org>
2022-07-05 17:59:21 -0400

    switch to node, postgres, Kysely for migrate

--- migtate2db.ts
+++ migtate2db.ts
@@ -1,74 +1,75 @@
-const offset = 0
+#!/usr/bin/env ts-node
 
-import { Database, MySQLConnector, Model, DataTypes } from 'https://deno.land/x/denodb/mod.ts';
+import { Generated, Kysely, PostgresDialect } from "kysely"
+import pkg from 'pg';
+const { Pool } = pkg;
+import { readFile, readdir as readDir} from 'node:fs/promises'
+import 'dotenv/config'
 
-const connection = new MySQLConnector({
-	host: '127.0.0.1',
-	database: 'letters',
-	//password: ommited for privacy,
-	//username: ommited for privacy,
-	port: 3306
-})
-
-const db = new Database(connection);
-
-class Letter extends Model {
-	static table = 'letters';
-	static fields = {
-		id: { 
-			type: DataTypes.INTEGER,
-		},
-		letter: DataTypes.TEXT,
-		date: {
-			type: DataTypes.DATETIME,
-			allowNull: true
-		},
-		lang: {
-			type: DataTypes.STRING,
-			length: 5,
-			allowNull: true
-		},
-		country: {
-			type: DataTypes.STRING,
-			length: 60,
-			allowNull: true
-		},
-		region: {
-			type: DataTypes.STRING,
-			length: 100,
-			allowNull: true	
-		}
-	}
+interface LtcBase {
+    lettermessage: string;
+    lettertags: string;
+    letterup: number;
+    letterlevel: number;
+    letterlanguage: string;
+    senderip: string;
+    sendercountry: string;
+    senderregion?: string;
+    sendercity?: string;
+    lettercomments: number
+    tofacebookuid?: number
+    fromfacebookuid?: number
 }
 
-await db.link([Letter])
-await db.sync()
+interface LtcTable extends LtcBase {
+    id: Generated<number>
+    letterpostdate: Date;
+}
 
-await Letter.delete()
+interface LtcJson extends LtcBase {
+    Id: number
+    letterPostDate: string
+}
 
-let i = 0
-for await (const file of Deno.readDir('./letter')) {
-	const n = Number(file.name.replace('.json',''))
-	if(n < offset) continue
-	console.log(n,i)
-	
-	const pfile: [
-		{
-			Id: number,
-			letterMessage: string,
-			letterPostDate: string,
-			letterLanguage: string
-		}] = JSON.parse(await Deno.readTextFile(`./letter/${file.name}`))
-	const data = pfile.map((entry) => {return {
-		id: entry.Id,
-		letter: entry.letterMessage.replaceAll(';','CHAR(59)'),
-		date: new Date(Number(entry.letterPostDate.replace('/Date(','').replace(')/',''))),
-		lang: entry.letterLanguage.replaceAll(';','CHAR(59)')
-	}})	
-	try {
-	await Letter.create(data)
-	} catch (_) {
-		console.log('looks like you got a duplicate in your dataset uwu')
-	}
-	i++
+interface Database {
+    ltc: LtcTable
+}
+
+let pool = new Pool({
+    database: process.env.db,
+    host: process.env.host || "localhost",
+    user: process.env.user,
+    password: process.env.pass
+})
+
+const db = new Kysely<Database>({
+    dialect: new PostgresDialect({
+        pool: pool
+    })
+})
+
+const files = await readDir('./letter')
+
+for (const file of files) {
+    console.log(file)
+    const contents = await readFile(`./letter/${file}`)
+    const json: LtcJson[] = JSON.parse(contents.toString())
+    
+    const patchedJson: LtcTable[] = json.map(letter => {
+        // @ts-expect-error
+        const newLetter: LtcTable = Object.fromEntries(
+            Object.entries(letter).map(([k, v]) => [k.toLowerCase(), v])
+        );
+        newLetter.lettermessage = newLetter.lettermessage.replaceAll(/[\x00]/g,'')
+        newLetter.letterpostdate = new Date(Number(letter.letterPostDate.split('/Date(')[1].split(')/')[0]))
+        return newLetter
+    })
+
+    await db
+        .insertInto('ltc')
+        .values(patchedJson)
+        .onConflict(oc => oc
+            .column('id')
+            .doNothing())
+        .execute()
 }
