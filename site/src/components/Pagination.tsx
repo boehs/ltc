366738e8fec49bbc1b07bdcefb09ab3f46b26cb8
Evan Boehs <evan@boehs.org>
2022-07-11 15:56:55 -0400

    fix 404 bug

--- site/src/components/Pagination.tsx
+++ site/src/components/Pagination.tsx
@@ -6,8 +6,8 @@ export default function Pagination(props: { id: number }) {
     return (<div class={pagination}>
         <a class={isActive(id() - 2)} href={`./${id() - 2}`}>{id() - 2}</a>
         <a class={isActive(id() - 1)} href={`./${id() - 1}`}>{id() - 1}</a>
-        <a class={isActive(id())} href={`/new/${id()}`}>{id()}</a>
-        <a href={`/new/${id() + 1}`}>{id() + 1}</a>
-        <a href={`/new/${id() + 2}`}>{id() + 2}</a>
+        <a class={isActive(id())} href={`./${id()}`}>{id()}</a>
+        <a href={`./${id() + 1}`}>{id() + 1}</a>
+        <a href={`./${id() + 2}`}>{id() + 2}</a>
     </div>)
 }
