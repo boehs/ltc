1e57658c20f6b15187907d8fa73328b81a213823
Evan Boehs <evan@boehs.org>
2022-07-10 22:57:40 -0400

    Commit letter (oops)

---
+++ site/src/routes/letter/[id].tsx
@@ -0,0 +1,52 @@
+import { useParams, useRouteData } from 'solid-app-router'
+import { db, getLocation } from '../../../../shared'
+import Letter from '~/components/Letter'
+import { createServerResource } from 'solid-start/server'
+import { IPv4 } from "ip-num/IPNumber.js";
+import { Resource, Show } from 'solid-js';
+import { main } from './[id].module.css'
+
+interface LetterData {
+    message: string;
+    id: number;
+    date: Date;
+    location: string;
+    commentsN: number;
+    hearts: number
+}
+
+export function routeData() {
+
+    return createServerResource(async () => {
+        const params = useParams()
+        const stuffs = await db
+            .selectFrom('ltc')
+            .where('id', '=', Number(params.id))
+            .limit(1)
+            .select(['lettermessage', 'id', 'letterpostdate', 'senderip', 'lettercomments', 'letterup'])
+            .executeTakeFirst()
+        return {
+            message: stuffs.lettermessage,
+            hearts: stuffs.letterup,
+            id: stuffs.id,
+            date: stuffs.letterpostdate,
+            location: getLocation(IPv4.fromString(stuffs.senderip)),
+            commentsN: stuffs.lettercomments
+        }
+    })
+
+}
+
+export default function LetterID() {
+    const data: Resource<LetterData> = useRouteData()
+    return (<main class={main}>
+        <Show when={data()}>
+            <Letter expanded={true} {...data()}/>
+        </Show>
+        <hr/>
+        <Show when={data()}>
+            <h3>{data().commentsN} comments</h3>
+        </Show>
+        
+    </main>)
+}
