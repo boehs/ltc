c220d7185bcb0a1066c96a792135c588c53afca6
Evan Boehs <evan@boehs.org>
2022-07-17 23:13:28 -0400

    jeez this file needed formatting

--- site/src/components/Header.tsx
+++ site/src/components/Header.tsx
@@ -6,22 +6,22 @@ import { isServer } from "solid-js/web";
 export default function Header() {
   const navigate = useNavigate()
   if (!isServer) {
-    createShortcut(['B'],() => history.back())
+    createShortcut(['B'], () => history.back())
   }
-  createShortcut(['R'],() => navigate('/random',{scroll: true}))
-  createShortcut(['N'],() => navigate('/new',{scroll: true}))
-  createShortcut(['S'],() => navigate('/search',{scroll: true}))
-  createShortcut(['F'],() => navigate('/',{scroll: true}))
-  
+  createShortcut(['R'], () => navigate('/random', { scroll: true }))
+  createShortcut(['N'], () => navigate('/new', { scroll: true }))
+  createShortcut(['S'], () => navigate('/search', { scroll: true }))
+  createShortcut(['F'], () => navigate('/', { scroll: true }))
+
   return (<header>
-            <h1>
-                <a href="/">letters to crushes</a>
-            </h1>
-            <ul>
-                <li><a class="secondary" href="/send">send</a></li>
-                <li><a class="secondary" href="/new">new</a></li>
-                <li><a class="secondary" href="/random">random</a></li>
-                <li><a class="secondary" href="/search">search</a></li>
-            </ul>
+    <h1>
+      <a href="/">letters to crushes</a>
+    </h1>
+    <ul>
+      <li><a class="secondary" href="/send">send</a></li>
+      <li><a class="secondary" href="/new">new</a></li>
+      <li><a class="secondary" href="/random">random</a></li>
+      <li><a class="secondary" href="/search">search</a></li>
+    </ul>
   </header>)
 }
