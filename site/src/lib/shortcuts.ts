59a0689f0de943aa3c43fffcd0c0e7800ce70b0f
Evan Boehs <evan@boehs.org>
2022-07-18 02:08:54 -0400

    hopefully fix shortcuts sometimes breaking.
    catch: beware, this doesn't make the situation any worse
    BUT the user can still *click* somewhere and lose their work

--- site/src/lib/shortcuts.ts
+++ site/src/lib/shortcuts.ts
@@ -1,9 +1,15 @@
 import { createShortcut as _createShortcut } from "@solid-primitives/keyboard"
 import { ReactiveSet } from '@solid-primitives/set'
+import { useIsRouting } from "solid-app-router"
+import { createMemo } from "solid-js"
 
 export const isTyping = new ReactiveSet()
 
 export const createShortcut: typeof _createShortcut = (...params) => {
+    createMemo(() => {
+        if (useIsRouting()) isTyping.clear()
+    }) 
+    
     const oldCallback = params[1]
     params[1] = () => {
         if (isTyping.size == 0 && !['INPUT','TEXTAREA'].includes(document.activeElement.tagName) ) oldCallback()
