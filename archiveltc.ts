2a5fbea627fff3c3aa346b37afdb318101b8c71a
Evan Boehs <evan@boehs.org>
2022-07-18 18:11:36 -0400

    grammer changes for archiver

--- archiveltc.ts
+++ archiveltc.ts
@@ -5,7 +5,7 @@ const workers: Worker[] = []
 const endpoints = [{
     endpoint: 'http://letterstocrushes.com/api/get_letters/-1',
     folder: 'letter',
-    offset: 68296
+    offset: 69297
 }
 //{
 //    endpoint: 'http://letterstocrushes.com/api/get_letters/-10',
