8f11030973888542c37917c06a302dd9a0b76ead
Evan Boehs <evan@boehs.org>
2022-07-30 00:10:50 -0400

    wrong place for the line-height css

--- site/src/components/Letter.tsx
+++ site/src/components/Letter.tsx
@@ -62,7 +62,7 @@ export default function Letter(props: LetterProps) {
                     </div>
                 </ErrorBoundary>
             </div>
-            <div innerHTML={xss(props.message)
+            <div class='letter-body' innerHTML={xss(props.message)
                 .replace(LetterLinkRegex, '<a href="/$1">letterstocrushes.com/$1</a>')
                 .replace(LetterHashRegex, '<a href="/$1$2">$1$2</a>')
                 .replaceAll('\n','<br/>')
