43c09c4e19a6127630fb9bbb4ec4c2ee8fd6ec26
Evan Boehs <evan@boehs.org>
2022-07-29 23:17:42 -0400

    idrk

--- site/src/routes/letter/[id].tsx
+++ site/src/routes/letter/[id].tsx
@@ -28,8 +28,6 @@ export function routeData({ params }) {
                 location: stuffs.senderip ? getLocation(IPv4.fromString(stuffs.senderip.trim())) : null,
                 commentsN: stuffs.lettercomments
             }
-        } else {
-            return false
         }
     })
 
@@ -37,9 +35,9 @@ export function routeData({ params }) {
 
 export default function LetterID() {
     const data: ReturnType<typeof routeData> = useRouteData()
-    createEffect(() => { if(data()) setActivePopup(false) })
+    createEffect(() => { if (data()) setActivePopup(false) })
     return (<>
-        <ErrorBoundary fallback={err => <RenderError error={err}/>}>
+        <ErrorBoundary fallback={err => <RenderError error={err} />}>
             <Show when={data()} fallback={<NotFound />}>
                 <Title>Letter {data().id}</Title>
                 <ErrorBoundary fallback={err => <RenderError error={err} />}>
@@ -47,29 +45,31 @@ export default function LetterID() {
                 </ErrorBoundary>
                 <ErrorBoundary fallback={err => <p>{err}</p>}>
                     <hr />
-                    <h3>{data().commentsN} comments</h3>
-                    <form>
-                        <Show when={!data().hidden} fallback={<>
-                            <h4>:/</h4>
-                            <p>You can't comment on hidden posts!</p>
-                        </>}>
-                            <h4>Leave a comment</h4>
-                            <label for="name">Name:</label>
-                            <input type="text" name="name" onInput={activeStateListener}></input>
-                            <label for="name">Email:</label>
-                            <input type="email" name="email" onInput={activeStateListener}></input>
-                            <small>Email is optional and never shown. Leave yours if you want email notifications on new comments for this letter.</small>
-                            <label for="comment">Leave a comment:</label>
-                            <textarea rows="1" name='comment' onInput={(e) => {
+                    <div class='comments'>
+                        <h3>{data().commentsN} comments</h3>
+                        <form>
+                            <Show when={!data().hidden} fallback={<>
+                                <h4>:/</h4>
+                                <p>You can't comment on hidden posts!</p>
+                            </>}>
+                                <h4>Leave a comment</h4>
+                                <label for="name">Name:</label>
+                                <input type="text" name="name" onInput={activeStateListener}></input>
+                                <label for="name">Email:</label>
+                                <input type="email" name="email" onInput={activeStateListener}></input>
+                                <small>Email is optional and never shown. Leave yours if you want email notifications on new comments for this letter.</small>
+                                <label for="comment">Leave a comment:</label>
+                                <textarea rows="1" name='comment' onInput={(e) => {
                                     activeStateListener(e)
                                     const target = e.target as HTMLTextAreaElement
                                     target.style.height = "1px";
                                     const height = target.scrollHeight + 2
-                                    target.style.height = (height > 192 ? 192 : height)+"px";
-                            }}></textarea>
-                            <input type='submit' value="Send 💌"></input>
-                        </Show>
-                    </form>
+                                    target.style.height = (height > 192 ? 192 : height) + "px";
+                                }}></textarea>
+                                <input type='submit' value="Send 💌"></input>
+                            </Show>
+                        </form>
+                    </div>
                 </ErrorBoundary>
             </Show>
         </ErrorBoundary>
