583f13ba039bab55eae808c5d4f2412c83bd7ab6
Evan Boehs <evan@boehs.org>
2022-07-31 19:50:41 -0400

    script "improvements"

--- toolkit/archive/archivedisqus.ts
+++ toolkit/archive/archivedisqus.ts
@@ -12,6 +12,7 @@ let params: {
     related: "thread",
     limit: 100,
     forum: "crushes",
+    cursor: "1264970782551154:0:0",
     api_key: process.env.API_KEY
 }
 
@@ -21,7 +22,7 @@ let params: {
 // http://mail.letterstocrushes.com/letter/
 // http://letterstocrushes.com/(F(Ie3anfATscfBdLjAV7KSIR7Ou7cjwphDJkYmQF0aVPNBGDqdmdMa_x-VsWUiHAKK90Sc71A4LznHuOtT_Ke4PnzsR6hUKRekOXxc8IozA-sfMK5r7lYDHBqs5HRzDOFZuJH5rJ6ArEMKoG484txuvzDFgnKnzIzkrYvIfhbeFGoCR9J_d3EY5haDKzYSx0n1bJD3DQSWeWsZj_X_cSM0yw2))/letter/461848
 
-const letterRegex = /https?:\/\/(([0-9.]*)|(((www\.)|(mail\.))?letterstocrushes\.com))\/(.*\/)?(([lL]etter)|(post))\//
+const letterRegex = /https?:\/\/(([0-9.]*)|((([Ww]ww\.)|(mail\.))?letterstocrushes\.com))\/(.*\/)?(([lL]etter)|(post))\//
 
 const createQueryString = (params: {
     [key: string]: any
@@ -42,13 +43,27 @@ async function* fetchAndMaybeAgain() {
 }
 
 for await (const response of fetchAndMaybeAgain()) {
-    console.log(response.response[0].id, response.response.length, response.cursor.next,response.response[0].thread.link)
     const body: any[] = response.response || []
     if (body.length > 0) {
         const l = body.flatMap(item => {
-            // crushed.tumblr.com/post/*
-                /(tumblr|post|disqus)/.test(item.thread.link as string)
+            const id = Number((item.thread.link as string)
+                // wtf
+                .replace('%202', '')
+                .replace(letterRegex, '')
+                // like ?refid=0
+                .replace(/\?.*/, '')
+            )
+
+            if (
+                // crushed.tumblr.com/post/*
+                // disqus.com/*
+                // crushes.disqus.com/*
+                // http://www.dhuck.com/letter/55775
+                /(tumblr|post|disqus|dhuck)/.test(item.thread.link as string)
+                // comments not on a letter
                 || !/(([lL]etter)|(post))\//.test(item.thread.link as string)
+                // (this one was hard deleted so there is a fk constraint violation)
+                || [36,23121,19660,19547,17450,14540,14257,13700,11141,10573].includes(id)
             )
                 return []
             // testing code
@@ -61,11 +76,7 @@ for await (const response of fetchAndMaybeAgain()) {
                 hearts: item.likes as number,
                 commentername: item.author.name as string,
                 commentdate: new Date(item.createdAt),
-                letterid: Number((item.thread.link as string)
-                    // wtf
-                    .replace('%202','')
-                    .replace(letterRegex, '')
-                ),
+                letterid: id,
                 // this is actually the commenter username
                 commenteremail: item.author.username as string,
                 // this is actually a custom input field
@@ -85,7 +96,8 @@ for await (const response of fetchAndMaybeAgain()) {
             }]
         })
         // @ts-expect-error
-        writeCommentsToDB(l)
+        if (l.length > 0) writeCommentsToDB(l)
+        console.log(response.response[0].id, response.response.length, response.cursor.next, response.response[0].thread.link,l.length)
     }
 }
 
